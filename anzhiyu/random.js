cf153150f01e8cad0ef942a05193f6fb77a42ce0
NiannianSoHungry <enoch_jiang@163.com>
2026-01-29 18:51:14 +0800

    Site updated: 2026-01-29 18:51:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/01/26/我目前做生信分析使用的文件树/","2026/01/27/ggplot2完全解析EP-01/"];function toRandomPost(){
+var posts=["2026/01/26/我目前做生信分析使用的文件树/","2026/01/27/ggplot2完全解析EP-01/","2026/01/29/使用ImageJ批量合并细胞荧光图像/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
