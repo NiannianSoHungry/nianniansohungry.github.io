1bc211d40c0f09642b7e9386a55caf9b77411b2e
Ubuntu <ubuntu@localhost.localdomain>
2026-06-11 01:06:23 +0800

    Site updated: 2026-06-11 01:06:22

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/01/27/ggplot2完全解析EP-01/","2026/01/29/使用ImageJ批量合并细胞荧光图像/","2026/01/26/我目前做生信分析使用的文件树/","2026/06/11/check-then-go-让AI先检查再动手/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
